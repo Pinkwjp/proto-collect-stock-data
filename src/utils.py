d0eb79c1f6b9d75f1a7450c8efca7e9974dfb265
ping <pinkwjp@gmail.com>
2025-04-12 11:35:28 -0400

    add func sp500_symbols

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+
+from typing import List
+from pathlib import Path
+
+import pandas as pd 
+
+
+
+def sp500_symbols() -> List[str]:
+    csv_file = Path('./index/sp500.csv')
+    assert csv_file.exists()
+    return  pd.read_csv(csv_file)['Symbol'].values.tolist()
+
+
