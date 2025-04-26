89b3e6e005943de6a98f3eb9fed91b251a87e906
ping <pinkwjp@gmail.com>
2025-04-26 12:35:07 -0400

    update func sp500_symbols

--- src/utils.py
+++ src/utils.py
@@ -9,8 +9,7 @@ import pandas as pd
 def sp500_symbols() -> List[str]:
     csv_file = Path('./index/sp500.csv')
     assert csv_file.exists()
-    symbols = pd.read_csv(csv_file)['Symbol'].values.tolist()
-    tickers = [s.replace('.', '-') for s in symbols]  # e.g BRK.B -> BRK-B
-    return tickers
+    return pd.read_csv(csv_file)['Symbol'].map(str).to_list()
+
 
 
