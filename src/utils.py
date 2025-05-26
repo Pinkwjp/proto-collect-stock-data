6f346acee22bbb45e64a8de632b3d0d9399dd8bd
ping <pinkwjp@gmail.com>
2025-05-26 12:19:37 -0400

    refactor download and cleaning data into one function - download_max

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,15 @@
 import getpass
 from typing import List
 from pathlib import Path
+from datetime import date
 
 import pandas as pd 
+
 from sqlalchemy import Engine
 from sqlalchemy import URL
 from sqlalchemy import create_engine
+import yfinance as yf
+
 
 
 
@@ -26,3 +30,46 @@ def get_db_engine() -> Engine:
     )
     return create_engine(url_object) 
 
+
+
+
+def download_max(symbols: List[str]) -> pd.DataFrame:
+    """download max stock price data for symbols
+    
+    start: first ever trade day of each symbol
+    end: the day before today (to aviod in-tradeday incomplete data)
+
+    NOTE: use this func to intially populate a sql table when records of symbols doesn't exist in table
+    """
+    stocks_df_raw = yf.download(tickers=symbols, group_by='Ticker', period='max', rounding=True)
+    assert stocks_df_raw is not None
+
+    # flatten multil-level columns
+    # https://stackoverflow.com/questions/63107594/how-to-deal-with-multi-level-column-names-downloaded-with-yfinance
+    stocks_df = stocks_df_raw.stack(level=0, future_stack=True).rename_axis(['Date', 'Ticker']).reset_index(level=1)
+
+    # lots of null value as all stocks first trading date set to match the oldlest stock's first trading date
+    df = stocks_df.dropna()
+
+    # change from float to int
+    df['Volume'] = df['Volume'].fillna(0).astype(int)
+
+    # change Date from index to column
+    df = df.reset_index() 
+
+    # to match df columns to sql table stock_prices columns
+    df = df.rename(columns={'Date': 'trade_date',    
+                            'Ticker': 'ticker',
+                            'Open': 'open_price',
+                            'High': 'high_price',
+                            'Low': 'low_price',
+                            'Close': 'close_price',
+                            'Volume': 'volume'
+                            })
+    
+    # remove today's stock data, as that could be incomplete if still within trading hour
+    if (to_day:= date.today()) == df['trade_date'].max():
+        df_today = df[df['trade_date'] == to_day] 
+        df = df.drop(index=df_today.index)
+    
+    return df
